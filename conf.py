0e47ed23f28ca72775d380d7316e50ef0c7a7bd2
chenrui <x14g1a@DESKTOP-VG7JEEL.localdomain>
2026-09-08 21:58:30 +0800

    1

--- conf.py
+++ conf.py
@@ -13,8 +13,11 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sphinx_rtd_theme
 
-extensions = []
+extensions = [
+        "sphinx_rtd_theme"
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
