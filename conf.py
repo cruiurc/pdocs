fbca35de2076d7bd460e116627ee10fa704ea80c
chenrui <x14g1a@DESKTOP-VG7JEEL.localdomain>
2026-09-09 09:09:00 +0800

    change theme

--- conf.py
+++ conf.py
@@ -7,17 +7,17 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pdocs'
-copyright = '2026, Admin'
-author = 'Admin'
+copyright = '2026, dev'
+author = 'dev'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
 extensions = [
-        "sphinx_rtd_theme"
-        ]
+# "sphinx_rtd_theme",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,6 +26,5 @@ language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'classic'
 html_static_path = ['_static']
