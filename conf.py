6d1dfd36e4cc58e26ea55a39c1876eb224224d3a
chenrui <x14g1a@DESKTOP-VG7JEEL.localdomain>
2026-09-09 09:30:17 +0800

    rtd theme

--- conf.py
+++ conf.py
@@ -13,10 +13,10 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 extensions = [
-# "sphinx_rtd_theme",
+ "sphinx_rtd_theme",
 ]
 
 templates_path = ['_templates']
@@ -26,5 +26,5 @@ language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
