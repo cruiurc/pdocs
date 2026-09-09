b7e65b3d721d4fa0d686268cbfde0cf97df74223
chenrui <x14g1a@DESKTOP-VG7JEEL.localdomain>
2026-09-09 11:00:41 +0800

    update conf.py and confirm succeeded

--- conf.py
+++ conf.py
@@ -14,9 +14,12 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import sphinx_rtd_theme
+import myst_parser
 
 extensions = [
- "sphinx_rtd_theme",
+ 'sphinx_rtd_theme',
+ 'myst_parser',
+ 'sphinxcontrib.jquery' 
 ]
 
 templates_path = ['_templates']
